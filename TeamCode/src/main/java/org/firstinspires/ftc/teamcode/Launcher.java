6268066fa4a3b0efd8879c51b35c2c241515b68a
pigedy100 <ruhosk@gmail.com>
2025-12-09 16:59:22 -0600

    I like where I can go with controls. Also I just remembered you can use rumble. I wonder if you can do the HD rumble equivalent on a PS5 controller.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Launcher.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Launcher.java
@@ -1,5 +1,25 @@
 package org.firstinspires.ftc.teamcode;
 
-public class Launcher {
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.ThermalEquilibrium.homeostasis.Controllers.Feedback.BasicPID;
+import com.ThermalEquilibrium.homeostasis.Parameters.PIDCoefficients;
+import com.qualcomm.robotcore.hardware.Gamepad;
 
+public class Launcher {
+    DcMotorEx main_motor;
+    boolean cruise_control;
+    byte speed;
+    Launcher(DcMotorEx m){main_motor = m;}
+    public void toggle_cruise_control(){cruise_control = !cruise_control;}
+    public void whats_next(Gamepad pad){
+        if (pad.aWasPressed()) toggle_cruise_control();
+        if (cruise_control){
+             if (pad.leftBumperWasPressed() && speed > 0) speed--;
+             else if (pad.rightBumperWasPressed() && speed < Byte.MAX_VALUE) speed++;
+             else {
+
+             }
+        }
+        else main_motor.setPower(pad.right_trigger);
+    }
 }
