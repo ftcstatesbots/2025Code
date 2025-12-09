dbb594cfc3fe1be9e12528e452f2ab6f02b040e4
pigedy100 <ruhosk@gmail.com>
2025-12-09 16:09:11 -0600

    Drivetrain.java pretty much finished

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Drivetrain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Drivetrain.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamcode.pedroPathing;
+package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.Gamepad;
@@ -10,9 +10,18 @@ import com.qualcomm.robotcore.hardware.Gamepad;
 3 - Left Front Motor */
 
 public class Drivetrain {
+    // A big update power function that updates power with one command
     public void master_update_power(Gamepad pad, DcMotorEx[] motors){
-
+        set_power(find_powers(find_stick_values(pad)), motors);
     }
+    // A SERIES OF SMALLER COMMANDS THAT CAN BE CALLED SEPARATELY
+
+    // Small set power command
+    public void set_power(double[] powers, DcMotorEx[] motors){
+        for (int i = 0; i < 5; i++) motors[i].setPower(powers[i]);
+    }
+
+    // Translates a series of stick values to usable power values
     public double[] find_powers(double[] val){
         return new double[]{
                 val[0] + val[1] - val[2],
@@ -21,6 +30,7 @@ public class Drivetrain {
                 val[0] + val[1] + val[2]
         };
     }
+    // Gives a list of input values given the gamepad
     public double[] find_stick_values(Gamepad pad){
         return new double[]{
              pad.left_stick_x,
