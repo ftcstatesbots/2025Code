6ff3cd71fabd67815561fdd0b8b135bff23fb6e5
pigedy100 <ruhosk@gmail.com>
2025-12-09 16:30:45 -0600

    Some housekeeping and setting up for more later

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOP.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOP.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamcode.pedroPathing;
+package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
@@ -7,19 +7,24 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 
 @TeleOp
 public class TeleOP extends OpMode {
+    DcMotorEx[] train_motors;
     DcMotorEx launch_motor, intake_motor,
             right_back_motor, right_front_motor,
             left_back_motor, left_front_motor;
+    Drivetrain main_train;
+    Launcher launcher;
 
     @Override
     public void init(){
         // Getting motors
         launch_motor = hardwareMap.get(DcMotorEx.class, "mEX_launch");
         intake_motor = hardwareMap.get(DcMotorEx.class, "mEX_intake");
-        right_back_motor = hardwareMap.get(DcMotorEx.class, "mEX_rightBack");
-        right_front_motor = hardwareMap.get(DcMotorEx.class, "mEX_rightFront");
-        left_back_motor = hardwareMap.get(DcMotorEx.class, "mEX_leftBack");
-        left_front_motor = hardwareMap.get(DcMotorEx.class, "mEX_leftFront");
+        train_motors = new DcMotorEx[]{
+            hardwareMap.get(DcMotorEx.class, "mEX_rightBack"),
+            hardwareMap.get(DcMotorEx.class, "mEX_rightFront"),
+            hardwareMap.get(DcMotorEx.class, "mEX_leftBack"),
+            hardwareMap.get(DcMotorEx.class, "mEX_leftFront")
+        };
 
         // Setting motor directions
         intake_motor.setDirection(DcMotor.Direction.REVERSE);
@@ -31,6 +36,7 @@ public class TeleOP extends OpMode {
         right_front_motor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         left_back_motor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         left_front_motor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        main_train = new Drivetrain(train_motors);
     }
     @Override
     public void loop(){
